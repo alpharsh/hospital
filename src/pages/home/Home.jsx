bced9b242d8228674a0abdb45d9273afb244eb79
Harsh Tripathi <alpharsh1@gmail.com>
2024-11-07 14:42:27 +0530

    Homepage UI chipset design

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,55 +11,102 @@ function Home() {
     <div className="">
       {/* HERO SECTION */}
       <div className="relative flex items-center justify-between w-full h-[47rem] bg-gradient-to-b from-[#002570] to-[#0074D1]">
-        <div>
-          {/* Left Section: Text Content */}
-        <div className="text-white  p-10 sm:pl-16 z-10">
-          <h1 className="flex items-end font-Bricolage-Grotesque text-7xl font-bold leading-tight mb-4">
-            We Provide Best <br />
-            Healthcare{" "}
-            <img src="/heart.png" alt="heart" className="absolute left-[30%]" />
-          </h1>
-          <p className="mb-8 font-Bricolage-Grotesque text-lg max-w-lg">
-            Explicabo esse amet tempora quibusdam laudantium, laborum eaque
-            magnam fugiat hic? Esse dicta aliquid error repudiandae earum
-            suscipit fugiat molestias, veniam, vel architecto veritatis delectus
-            repellat modi impedit sequi.
-          </p>
-          <a
-            href="#"
-            className="inline-flex items-center px-9 py-2 text-xl bg-white text-black font-Bricolage-Grotesque font-bold rounded-full shadow-lg hover:bg-gray-100 transition duration-300 ease-in-out"
+        <div className="absolute left-0 top-0 z-10">
+          <svg
+            width="451"
+            height="451"
+            viewBox="0 0 451 451"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            More About{" "}
-            <span className="ml-2 text-blue-600">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={2}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17 8l4 4m0 0l-4 4m4-4H3"
-                />
-              </svg>
-            </span>
-          </a>
+            <path
+              d="M447.374 147.228C476.019 251.079 328.819 446.129 221.208 450.877C97.1296 456.351 -34 278.472 -34 147.228C-34 15.9843 8.94081 -39.3531 115.604 -56.0846C245.462 -76.4543 426.254 70.6557 447.374 147.228Z"
+              fill="#1C5FCB"
+              fill-opacity="0.61"
+            />
+          </svg>
         </div>
+        <div className="z-10">
+          {/* Left Section: Text Content */}
+          <div className="text-white  p-10 sm:pl-16 z-10">
+            <h1 className="flex items-end font-Bricolage-Grotesque text-7xl font-bold leading-tight mb-4">
+              We Provide Best <br />
+              Healthcare{" "}
+              <div className="absolute left-[28rem]">
+                <img src="/heart.png" alt="heart" />
+              </div>
+            </h1>
+            <p className="mb-8 font-Bricolage-Grotesque text-lg max-w-lg">
+              Explicabo esse amet tempora quibusdam laudantium, laborum eaque
+              magnam fugiat hic? Esse dicta aliquid error repudiandae earum
+              suscipit fugiat molestias, veniam, vel architecto veritatis
+              delectus repellat modi impedit sequi.
+            </p>
+            <a
+              href="#"
+              className="inline-flex items-center px-9 py-2 text-xl bg-white text-black font-Bricolage-Grotesque font-bold rounded-full shadow-lg hover:bg-gray-100 transition duration-300 ease-in-out"
+            >
+              More About{" "}
+              <span className="ml-2 text-blue-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                  />
+                </svg>
+              </span>
+            </a>
+          </div>
 
-        {/* Right Section: Doctor Image */}
-        <div className="absolute right-0 bottom-0 h-full">
-          <img src="/hero.png" alt="Doctor" className="h-full object-cover" />
-        </div>
+          <div className="absolute right-32 top-0">
+            <svg
+              width="485"
+              height="509"
+              viewBox="0 0 485 509"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M481.374 205.228C510.019 309.079 362.819 504.129 255.208 508.877C131.13 514.351 0 336.472 0 205.228C0 73.9843 42.9408 18.6469 149.604 1.91543C279.462 -18.4543 460.254 128.656 481.374 205.228Z"
+                fill="#1C5FCB"
+                fill-opacity="0.61"
+              />
+            </svg>
+          </div>
+          {/* Right Section: Doctor Image */}
+          <div className="absolute right-0 bottom-0 h-full">
+            <img src="/hero.png" alt="Doctor" className="h-full object-cover" />
+          </div>
         </div>
       </div>
 
       {/* CARDS SECTION */}
-      <div className=" py-12 bg-gradient-to-b from-[#0074D1] to-white ">
+      <div className="pt-0 py-12 bg-gradient-to-b from-[#0074D1] to-white relative custom-shadow">
+        <div className="absolute left-[30rem] -top-60 z-0">
+          <svg
+            width="485"
+            height="509"
+            viewBox="0 0 485 509"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M481.374 205.228C510.019 309.079 362.819 504.129 255.208 508.877C131.13 514.351 0 336.472 0 205.228C0 73.9843 42.9408 18.6469 149.604 1.91543C279.462 -18.4543 460.254 128.656 481.374 205.228Z"
+              fill="#1C5FCB"
+              fill-opacity="0.61"
+            />
+          </svg>
+        </div>
         {/* Achievement Card */}
-        <div className="flex lg:max-w-[1200px] absolute top-[42.75rem] left-[10rem] items-center justify-between w-4/5 mx-auto bg-white shadow-lg rounded-[3rem] py-6 px-8">
+        <div className="flex lg:max-w-[1200px] items-center justify-between w-4/5 mx-auto bg-white shadow-2xl shadow-[#0074D1] rounded-[3rem] py-6 px-8 relative z-10">
           {/* Image Section */}
           <div className="flex-shrink-0">
             <img
