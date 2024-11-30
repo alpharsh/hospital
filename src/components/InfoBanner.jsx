9f3869d25380395d70a6f130c7d60fc744662fba
Harsh Tripathi <alpharsh1@gmail.com>
2024-11-30 20:27:47 +0530

    Refine InfoBanner layout by adjusting padding and font sizes for improved readability

--- src/components/InfoBanner.jsx
+++ src/components/InfoBanner.jsx
@@ -9,17 +9,17 @@ const InfoBanner = () => {
       <div className="container lg:max-w-[1200px] mx-auto w-[90%] md:w-5/6 flex flex-col md:flex-row items-center justify-between md:px-6">
         {/* Left Section: Icon and Text */}
         <div className="flex items-center space-x-4 md:space-x-6">
-          <div className="bg-white text-blue-500 rounded-full p-3 md:p-8">
+          <div className="bg-white text-blue-500 rounded-full p-3 md:p-5">
             <CiMedicalCase className=' text-2xl md:text-5xl'/>
           </div>
-          <p className="text-[0.8rem] md:text-4xl font-semibold leading-tight font-Bricolage-Grotesque md:w-[42rem]">Since 2007 We’re working with Medical group more than 1200+ Patients</p>
+          <p className="text-[0.8rem] md:text-3xl font-semibold leading-tight font-Bricolage-Grotesque md:w-[42rem]">Since 2007 We’re working with Medical group more than 1200+ Patients</p>
         </div>
 
         {/* Right Section: Contact Us Button */}
         <div className="mt-2 md:mt-0">
-          <Link to="/contact" className="bg-white text-black hover:bg-blue-100 px-2 md:px-9 py-1 md:py-3 rounded-full flex items-center space-x-2">
-            <span className='font-semibold md:font-bold text-xs md:text-xl'>Contact Us</span>
-            <FaPhoneAlt className='text-xs md:text-2xl text-blue-500'/>
+          <Link to="/contact" className="bg-white text-black hover:bg-blue-100 px-2 md:px-5 py-1 md:py-2 rounded-full flex items-center space-x-2">
+            <span className='font-semibold font-Bricolage-Grotesque text-xs md:text-xl'>Contact Us</span>
+            <FaPhoneAlt className='text-xs md:text-xl text-blue-500'/>
           </Link>
         </div>
       </div>
